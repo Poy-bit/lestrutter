410058b5441d015cbf3911d7d9938d9aa258da67
malserweins <81694516+Poy-bit@users.noreply.github.com>
2023-07-16 23:32:54 +0800

    Draggable Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 #include <SDL.h>
 #include "glad/glad.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <string>
+#include "shader.h"
+
+using namespace glm;
+
+const float WIN_WIDTH = 800;
+const float WIN_HEIGHT = 600;
+
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO); // TODO: Add to tests
 
@@ -12,7 +22,7 @@ int main(int argc, char* argv[]) {
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-	SDL_Window* window = SDL_CreateWindow("Lestrutter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("Lestrutter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
 	SDL_GLContext gl_context = SDL_GL_CreateContext(window); // TODO: Add to tests
 	SDL_GL_MakeCurrent(window, gl_context);
@@ -39,21 +49,38 @@ int main(int argc, char* argv[]) {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	shader mainShader = shader(SOURCE_PATH+std::string("/res/shaders/vert.txt"), SOURCE_PATH+std::string("/res/shaders/frag.txt"));
+	mainShader.bind();
+
+	vec2 origin = {0, 0};
+	vec2 origin_campos = { 0, 0 };
+	vec2 campos = {0, 0};
+
+	bool pressed = false;
+
 	SDL_Event e;
 	bool quit = false;
 	while (!quit) {
 		while (SDL_PollEvent(&e) != 0) {
-			if (e.type == SDL_QUIT) {
-				std::cout << "QUIT\n";
-				quit = true;
-			} else if (e.type == SDL_TEXTINPUT) {
-				int x = 0, y = 0;
-				SDL_GetMouseState(&x, &y);
-				std::cout << e.text.text[0];
-				//handleKeys(e.text.text[0], x, y);
+			switch (e.type) {
+			case (SDL_QUIT): { quit = true; break; }
+			case (SDL_MOUSEBUTTONDOWN): {
+				origin = { e.button.x / (WIN_WIDTH/2)-1, 1-e.button.y / (WIN_HEIGHT/2)-1 };
+				pressed = true;  break;
+			} case (SDL_MOUSEBUTTONUP): {
+				origin_campos = campos;
+				pressed = false;  break;
+			} case (SDL_MOUSEMOTION): {
+				if (pressed) {
+					vec2 rel_pos = vec2(e.motion.x / (WIN_WIDTH / 2) - 1, 1-e.motion.y / (WIN_HEIGHT / 2) - 1);
+					campos = origin_campos + (rel_pos - origin); }
 			}
-			
+			}
 		}
+
+		mat4 cammat = translate(mat4(1.0f), vec3(campos, 0.0f));
+		mainShader.setMat4("cammat", cammat);
+
 		glClear(GL_COLOR_BUFFER_BIT);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
